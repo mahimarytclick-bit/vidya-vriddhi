dc6cdb5e6cb7fc9880e54fcf534fe210827b3939
mahima <mahima.rytclick@gmail.com>
2026-03-13 16:25:52 +0530

    compononant-replace

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import Hero from "@/components/home/hero";
-import TopColleges from "@/components/home/topColleges";
-import Courses from "@/components/home/courses";
+import Hero from "@/app/components/home/hero";
+import TopColleges from "@/app/components/home/topColleges";
+import Courses from "@/app/components/home/courses";
 export default function Home() {
   return (
     <div >
